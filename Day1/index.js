b9126ec891871e44c0f31ead50f3fddb10c12d92
Rahul Kmanoj <kamo.rahul@gmail.com>
2017-10-25 13:06:32 +0530

    first day

--- Day1/index.js
+++ Day1/index.js
@@ -12,7 +12,62 @@ console.log(process.argv)
 
 var abc = "asfsdf"
 
-abc = [{},{},{},"egsdf"]
+var abc = [{},{},{},"egsdf"]
+
+
+//Primitive
+//Number
+var abc = 0
+
+
+// String
+var abc = "" 
+
+//Float
+var abc = 0.5
+
+
+//Reference 
+
+//Object
+var abc  = {};
+
+var abc = new Object();
+
+
+//Prototype Chain
+abc.toString()
+
+
+
+var abc  = {"xyz" : "string", "toString" : "sdkfbghsd"};
+
+
+
+abc.toString();
+
+
+
+abc.skjfbj
+
+abc.hasOwnProperty("xyz");
+
+
+
+
+var arr = ["asdf","sdf","sadfsd"];
+
+var obj = {abc : "atruf" , abc1 : "atruwerf" ,abc3 : "atrweruf" ,abc4 : "atrusff" ,abc6 : "asdftruf" ,}
+
+
+
+
+for (var i = 0; i < arr.length; i++) {
+	console.log(arr[i])
+}
+
+
+
 
 
 
