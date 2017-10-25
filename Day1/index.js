673fa2f249f5ce028c3af22f776edd7c6976f1ca
Rahul Kmanoj <kamo.rahul@gmail.com>
2017-10-25 12:07:32 +0530

    first day

---
+++ Day1/index.js
@@ -0,0 +1,38 @@
+// No New line char 
+
+// process.stdout.write("Hello")
+
+// console.log("New Line Char")
+
+
+
+// console.log(process)
+
+console.log(process.argv)
+
+var abc = "asfsdf"
+
+abc = [{},{},{},"egsdf"]
+
+
+
+
+
+// global.a = "abc"
+
+
+// console.log(a)
+
+
+
+
+// var abc = {a : "string"};
+
+// function_name(abc)
+// console.log(abc);
+// // var obj = {};
+
+
+// function function_name(argument) {
+// 	argument = {a : "string123",b : "123"}
+// }
