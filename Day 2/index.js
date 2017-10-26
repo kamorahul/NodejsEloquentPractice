7b23b73663bcd8b5bb2580795b69aa0604198473
Rahul Kmanoj <kamo.rahul@gmail.com>
2017-10-26 11:28:46 +0530

    second day

---
+++ Day 2/index.js
@@ -0,0 +1,21 @@
+var num = 0
+
+var abc = {
+	"a" : "sdfsdf",
+	"wefsa" : num ,
+
+}
+
+
+abc["a"]
+
+
+
+var obj = {"a" : "wds","b": "sdfgdfg",3 : "wefasdf",4 : {d: "sadf",c :{"abc" : [{"asefd" : "sedfgdfgsdfg"}]}}}
+
+
+// obj["4"].c.abc[0].asefd
+
+
+
+console.log(Object.indexOf)
