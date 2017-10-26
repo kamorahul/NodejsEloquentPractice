47e203f6646d8955f1251abf6c424e31897bb359
VikasKaloti <33083019+VikasKaloti@users.noreply.github.com>
2017-10-26 17:05:30 +0530

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,90 @@
+var cv={
+	"PERSONAL_DETAILS":{"Name":"Vikas Kaloti","D.O.B.":"11th_november_1996","E_mail":"vikaskaloti3@gmail.com","MobileNo":8054895868},
+	"ACADEMIC":{
+		"qual":[
+		         {"Year":2018,"Degree":"B.tech","Institute":"LPU","per":78.12},
+		         {"Year":2014,"Degree":"Intermediate","Institute":"GNM     Sen.Sec.School","per":83.33},
+		         {"Year":2012,"Degree":"High-School","Institute":"Govt.High School","per":88.54}
+				]
+			},
+	"SKILL_SET":{
+		"Programming_Languages":["c","c++","java"],
+        "IDE":["Intellij","Devcpp","Eclipse"],
+		"Operating_Systems":["Ubuntu_17.04","Red_Hat_Linux_9.0","Windows_10"],
+        "Databases":"MySQL"
+    },
+	"PROJECTS":
+	[
+	 {"Sudoku":{
+		"Description":"Application based on java that provide us an interface to play sudoku game and check the correctness.",
+		"Duration":"Mar_2016_to_Apr_2016"
+                             }
+	 },
+	 {
+	   "cgpa_calculator":{
+		   "Description":"Developed This using Math library of Java which calculates the cgpa according to the grades.",
+		   "Duration":"Feb_2017_to_Mar 2017"
+	   }
+	 }
+	
+	],
+	"ACHIEVEMENTS":
+	[
+	  "Completed Java Master Class with Tim Buchalka- Udemy","participated in inter-district state level games at Fatehgarh sahib",
+      "bagged 1st position in school level test conducted by GNM institution"	  
+	],
+	"LANGUAGE_PROFICIENCY":
+	[
+	  "English","Hindi","Punjabi"
+	]
+		
+};
+
+
+
+var obj={
+	"SKILL_SET":{
+		"Programming_Languages":["c","c++","java"],
+        "IDE":["Intellij","Devcpp","Eclipse"],
+		"Operating_Systems":["Ubuntu_17.04","Red_Hat_Linux_9.0","Windows_10"],
+        "Databases":"MySQL"
+    }
+}
+
+var obj={
+"PROJECTS":
+	[
+	 {"Sudoku":{
+		"Description":"Application based on java that provide us an interface to play sudoku game and check the correctness.",
+		"Duration":"Mar_2016_to_Apr_2016"
+                             }
+	 },
+	 {
+	   "cgpa_calculator":{
+		   "Description":"Developed This using Math library of Java which calculates the cgpa according to the grades.",
+		   "Duration":"Feb_2017_to_Mar 2017"
+	   }
+	 }
+	
+	]
+	
+}
+
+var obj={
+"ACHIEVEMENTS":
+	[
+	  "Completed Java Master Class with Tim Buchalka- Udemy","participated in inter-district state level games at Fatehgarh sahib",
+      "bagged 1st position in school level test conducted by GNM institution"	  
+	]
+	
+}
+
+var obj={
+	"LANGUAGE_PROFICIENCY":
+	[
+	  "English","Hindi","Punjabi"
+	]
+}
+
+
+
