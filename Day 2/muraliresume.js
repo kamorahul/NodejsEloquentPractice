ceb835197d6be1cf14e9431614f6b0f5c2017c59
Murali <muralisai.ch@gmail.com>
2017-10-26 16:50:35 +0530

    murali

---
+++ Day 2/muraliresume.js
@@ -0,0 +1,21 @@
+var resume = {"Name": "Chiranji Muralisaiganesh",
+ "Gender": "Male", 
+"Email": "muralisai.ch@gmail.com", 
+"Mobile": "8008079679", 
+"Academic" :[{"year": "2018","Degree": "B.Tech(CSE)","Institute": "Lovely professional university","Percentage": "7.53"},{"year": "2014","Degree": "XII","Institute": "Sri Chaitanya Educational Institutions","Percentage": "89.6"},{"year": "2012","Degree": "X","Institute": "Bhashyam public school","Percentage": "8.7"}], 
+"Projects":[{"Event_Reminder": "I developed this application using Android Studio. It alerts about important events of the user using reminder system. This application uses alarm and notification manager of the android system."},{"Analysis_of_Indian_Tourism_on_Gdp": "I did an analysis on the data regarding Indian tourism and how it effects Gross Domestic Product of India from 1999 to 2016. In this I made analysis of the data using different types of graphs and charts which helps for better understanding."}], 
+"Position_Of_Responsibility": [{"2014-2015":"Elected as the Class representative"}, {"2010-2011":"Captain of Cricket Team in school-level."}],
+"Extra_Curricular":
+ [{"Certifications":"Certified by Infopark for completing java course."}, {"Achievements":"Won 2nd prize in Inter school Cricket tournament in the year 2010."}],
+"Language_Proficiency" :["English","Telugu","Hindi"], 
+"personal_details" : {"Date_of_birth":"31 march 1996", "Nationality": "Indian", "Address":"13-138, Ferry road, Ibrahimpatnam, Vijayawada, Andhra Pradesh – 521456."}}
+
+
+
+
+console.log(resume)
+console.log(resume.Academic[1].Institute)
+console.log(resume.personal_details.Nationality)
+
+
+
