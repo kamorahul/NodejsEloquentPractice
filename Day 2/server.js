2bf419d634e4e0e6ac4ca14e8c3748a4115bd36b
sidsahu172 <siddharth.sahu172@gmail.com>
2017-11-01 17:36:46 +0530

    Login server
    username: siddharth
    password: abc123

---
+++ Day 2/server.js
@@ -0,0 +1,36 @@
+var http=require('http');
+var data=require('./data');
+
+//Creating server
+
+http.createServer(function(req,res){
+var link=req.url;
+  if(req.url==='/'){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Welcome Home");}
+
+  else if(req.url.match(/[/]login[?][$][a-z]*[=][A-Za-z0-9]*&[$][A-Za-z0-9]*=[A-Za-z0-9]*/g)){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+
+
+
+var z=link.substring(link.search(/[A-Za-z0-9]*[&]/g));
+var user=z.substring(0,z.indexOf('&'));
+var y=z.substring(z.search(/[&]/g));
+var pass=y.substring(y.indexOf("=")+1);
+
+if(user==data.username && pass==data.password){
+res.end("Login successful")
+  }
+  else if(user!=data.username && pass==data.password){
+  res.end("Oops!! wrong username")
+    }
+
+    else if(user==data.username && pass!=data.password){
+    res.end("Oops!! wrong password")
+      }
+      else {
+        res.end("User does not exist.")
+      }
+}
+}).listen(3000);
