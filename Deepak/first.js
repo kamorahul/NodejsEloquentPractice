1b7e97c78a02f48da0820f35817a180b437fb77d
Deepak Chauhan <0000deepak0000@gmail.com>
2017-11-03 15:57:40 +0530

    Online Console

---
+++ Deepak/first.js
@@ -0,0 +1,25 @@
+var abc=0;
+/*var obj={};
+
+fun(abc);
+console.log(abc);
+
+function fun(arg){
+	arg=50;
+}
+
+var arr=[10,{},"hi",undefined];	
+console.log(arr);
+
+console.log(process.argv);
+
+var o= new Object();
+*/
+
+//var a={"prop":"xyz"};
+//console.log(a.prop);
+
+//console.log(a.hasOwnProperty("prop"));
+
+var arr=[10,{},"hi",undefined];
+console.log(arr[1]);
