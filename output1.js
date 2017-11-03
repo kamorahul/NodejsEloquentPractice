ffc9d6b4dc82d32a08dbafcd4bbad0fc20f8ed3a
ChandraShekarReddy1996 <33084387+ChandraShekarReddy1996@users.noreply.github.com>
2017-11-03 15:13:03 +0530

    using child spawn

---
+++ output1.js
@@ -0,0 +1,23 @@
+///for every refresh the value is appended
+the sum of a and b is  9
+the sum of a and b is  9
+the sum of a and b is  9
+the sum of a and b is  10
+the sum of a and b is  10
+the sum of a and b is  10
+
+
+////if we give an error this will be done
+/home/chandra/Desktop/input.js:3
+console.log("the sum of a and b is ", aa+ b) 
+                                      ^
+
+ReferenceError: aa is not defined
+    at Object.<anonymous> (/home/chandra/Desktop/input.js:3:39)
+    at Module._compile (module.js:410:26)
+    at Object.Module._extensions..js (module.js:417:10)
+    at Module.load (module.js:344:32)
+    at Function.Module._load (module.js:301:12)
+    at Function.Module.runMain (module.js:442:10)
+    at startup (node.js:136:18)
+    at node.js:966:3
