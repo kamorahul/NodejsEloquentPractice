25164dce36a752667d77789dbcd10a1f5983044c
Mayank <33113205+Mayank-clicklabs@users.noreply.github.com>
2017-11-09 17:24:13 +0530

    Update mongoosee.js

--- mongoosee.js
+++ mongoosee.js
@@ -1,4 +1,93 @@
 //schema created down
+ function validate(schema,data){
+    
+    var sch_prop = schema.properties;
+    
+    var data_rev=schema.required;
+    
+    var attributes=Object.keys(sch_prop);
+    
+    var val_key = Object.keys(data);
+    
+    function contains(array, item) {
+    
+    for (var i = 0; i < array.length; i++) {``
+    
+    if (array[i] === item) {
+    
+    return true;
+    
+    }
+    
+    }
+     
+    return false;
+    
+    }
+    
+    //check for required fields
+    
+    for(var i=0;i<data_rev.length;i++){
+    
+    if(! contains(val_key,data_rev[i])){
+    
+    console.log("Required field is not entered");
+    
+    return false;
+    
+    }
+    
+    else if(data[data_rev[i]] == undefined){
+    
+    console.log("Required field is undefined");
+    
+    return false;
+    
+     
+    
+    } 
+    
+    }
+    
+    //check for extra data
+    
+    for(var i=0;i<val_key.length;i++){
+    
+    if(! contains(attributes,val_key[i])){
+    
+    console.log("You are entering extra field");
+    
+    return false;
+    
+    } 
+    
+    }
+    
+    //check data type
+    
+    for(var i=0;i<attributes.length;i++){
+    
+    var sch_field =sch_prop[attributes[i]];
+    
+    var sch_field_key=Object.keys(sch_field); 
+    
+    if(typeof data[attributes[i]]!=sch_field[sch_field_key[0]]){
+    
+    console.log("Incorrect Data Type"); 
+    
+    return false;
+    
+    } 
+    
+    }
+     
+    return true;
+    
+    }
+
+
+
+
 
 function create (user,constrains) {
         var fs = require('fs');
@@ -30,10 +119,8 @@ function create (user,constrains) {
         MongoClient.connect(url,function(err, db) {
             if (err) throw err;
             else{
-                var Ajv= require('ajv');
-                var ajv=Ajv({allErrors:true});
-                var validate = ajv.compile(cons);
-                if(validate(values)){
+                
+                if(validate(cons,values)){
                     db.collection(user).insertOne(values, function(err, res) {
                         if (err) throw err;
                         console.log("1 User added");
@@ -43,7 +130,7 @@ function create (user,constrains) {
                     });
                 }
                 else{
-                    console.log("invalid USER");
+                    console.log(" USER_Data is invalid");
                 }
           
             }
@@ -56,5 +143,5 @@ function create (user,constrains) {
 
     
  
-   //create("schema",{"properties" : {"name" : {"type":"string"},"age" : {"type":"integer"}},"required":["name"]};
+   //create("schema",{name:{type:string,required:true},age:{type:number,required:true}});
     insert("schema",{name:"abc",age:21});
