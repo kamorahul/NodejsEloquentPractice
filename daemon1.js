fb195bd1301860b2d2e78d46980cd14ee47d761c
Vishal <vishalsaraf44@gmail.com>
2017-11-10 11:33:15 +0530

    Nodemon Updated for Server

--- daemon1.js
+++ daemon1.js
@@ -1,26 +1,32 @@
 var fs=require('fs');
 var testFolder = './';
-check(testFolder);
+var http=require('http');
+http.createServer((req,res)=>{
+
+  check(testFolder);
+
+}).listen(3000);
 
 function check(testFolder)
 {
+  console.log("hello");
 fs.readdir(testFolder, (err, files) => {
   files.forEach((file) => {
     if(file.match(".js")!=null)
     {
       fs.watchFile(testFolder+file, (curr, prev) => {
-        console.log(`file : `+testFolder+file+` is modified at ${curr.mtime}`);
+        //console.log(/*`file : `+testFolder+*/file+` is modified at ${curr.mtime}`);
+        console.log("Server Restarted because "+file+" is modified");
       });
     }
     else
     {
       var temp=file.match("[a-z0-9]+.[a-z0-9]+");
-
       if(temp[0]==file){
-        console.log("other files : "+file);
+        //console.log(/*"other files : "+*/file+" is not added because this is not a .js file");
       }
       else{
-        console.log("directory :"+ file);
+        //console.log("directory :"+ file);
         check(testFolder+file+"/");
       }
     }
