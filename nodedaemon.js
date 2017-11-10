ac83015037c10e3d464127773ab8bf7fbaaee3d7
Ranjith kasu <33082853+ranjithkasu@users.noreply.github.com>
2017-11-10 21:15:14 +0530

    Add files via upload

---
+++ nodedaemon.js
@@ -0,0 +1,35 @@
+var fs=require('fs');
+var http=require('http');
+var testFolder = './';
+http.createServer((req,res) =>{
+  check(testFolder);
+
+}).listen(3000);
+
+
+
+function check(testFolder)
+{
+fs.readdir(testFolder, (err, files) => {
+  files.forEach((file) => {
+    if(file.match(".js")!=null)
+    {
+      fs.watchFile(testFolder+file, (curr, prev) => {
+        console.log(`file : `+testFolder+file+` is modified at ${curr.mtime}`);
+      });
+    }
+    else
+    {
+      var temp=file.match("[a-z0-9]+.[a-z0-9]+");
+
+      if(temp[0]==file){
+       console.log("other files : "+file);
+      }
+      else{
+        console.log("directory :"+ file);
+        check(testFolder+file+"/");
+      }
+    }
+  });
+  })
+}
