3240aab1d0e21f204c50f9c104200b7ebdcb4560
Rajpal Singh <srajpal206@gmail.com>
2017-11-11 23:25:14 +0530

    Update mongoose2.js

--- mongoose2.js
+++ mongoose2.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:1000/mydb';
-MongoClient.connect(url, function(err,db){
+var mongo = function(url){
+    MongoClient.connect(url, function(err,db){
     if (err)
         throw err;
     db.createCollection("myinfo").insert(input, function(err, res){
@@ -17,6 +18,7 @@ MongoClient.connect(url, function(err,db){
     });
     db.close();
 });
+}
 
 var keyInput = Object.keys(input);
 var keySchema = Object.keys(schema);
@@ -24,15 +26,15 @@ var keySchema = Object.keys(schema);
 if(keySchema.length>=keyInput.length){
     for(var i=0;i<keySchema.length;i++){
         var data = input[keyInput[i]];
-        data = String(inp);
+        data = String(data);
         var regExp = schema[keySchema[i]]["type"];
         regExp = new RegExp(regExp);
-        if(regExp.test(inp)==false){
+        if(regExp.test(data)==false){
             console.log("Did not match");
             break;
         }
         if(i==(keySchema.length-1)){
-            myMongo(url);
+            mongo(url);
         }
     }
 
