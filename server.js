a550fe1ff945a779d30c168e541c0a8213e967a5
ajeet5zx <ajeet.ajeet.aks@gmail.com>
2017-11-16 01:38:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ users=[];
 connections=[];
 server.listen(process.env.PORT || 5000);
 console.log("oh yeh server is running!!");
-console.log("bc1");
+//console.log("bc1");
 app.get('/', function(req, res)
 {
     res.sendFile(__dirname+'/index.html');
 });
 //console.log("bc");
 io.sockets.on('connection',function(socket)
-{   console.log("bc");
+{  // console.log("bc");
     connections.push(socket);
     console.log('%s connection conected',connections.length);
     
@@ -28,14 +28,14 @@ io.sockets.on('connection',function(socket)
     });
 
     socket.on('send message', function(data)
-    {   console.log(data);
+    {  // console.log(data);
         io.sockets.emit('new message',{msg:data,user:socket.username});
 
     });
 
    
     socket.on('new user',function(data,callback){
-        console.log(data);
+        //console.log(data);
         callback(true);
         socket.username=data;
         users.push(socket.username);
