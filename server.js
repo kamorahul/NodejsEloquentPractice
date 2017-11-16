3c7a42a14628fc299f16ae2d27d66554f1cb4773
ramnivas <dhaker.raj55@gmail.com>
2017-11-16 12:52:51 +0530

    chat app updated by Ramnivas

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-var http=require('http');
-var url=require('url');
-var matchFromReg=require('./parseFromUrl.js');
-var fs=require('fs');
-var detail=require('./detail.json');
-var qs=require('querystring'); //used for html page
-var server=http.createServer((req,res)=>{
-    var pathparse=url.parse(req.url).pathname;
-    if(req.method=="GET")  // using postman url like localhost:2000/username=ram&password=1234
-     {
-     console.log("welcome");
-     console.log(pathparse);
-     console.log(matchFromReg.decode(pathparse));
-       res.writeHead(200, {'Content-Type':'text/html'});
-       res.write(matchFromReg.decode(pathparse));//gives the url pathname
-       res.end();
 
-     }
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var moment=require('moment');
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/login.html');
 });
 
 
-server.listen(2000);
+var connectedUser=[];
+
+io.on('connection', function(socket){
+  var name;
+  
+
+    socket.on('newUser',(obj)=>{
+     name=obj.name;
+     console.log(name ," is connected ")
+     connectedUser.push(name);
+     socket.broadcast.emit("chat message","a new user "+name+"  is connected");
+    });
+   
+
+    socket.on('chat message', function(msg){
+        console.log( msg);
+  
+      socket.broadcast.emit("chat message",moment().calendar()+" : "+msg)
+    });
+
+    socket.on('disconnect', function(){
+      
+      console.log(name+" user disconnect:" )
+
+      var index=connectedUser.indexOf(name);
+      connectedUser.splice(index,1);
+      console.log("total connected user is :",connectedUser) 
+
+      socket.broadcast.emit("chat message"," "+name+" is disconnected.")
+    
+
+    });
+  });
+http.listen(5000, function(){
+  console.log('listening on *:5000');
+});
