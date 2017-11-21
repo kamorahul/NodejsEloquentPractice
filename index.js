31676fefb16f789e682aa13ff4fe04b4bf8f7883
Shamik Chatterjee <shamik.prithu@gmail.com>
2017-11-21 12:49:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-/*var app=require('express')();
-var http=require('http').Server(app);
-var io=require('socket.io')(http);
-users=[];
-connections=[];
-app.get('/', function(req,res){
-    res.sendFile(__dirname+'/index.html');
-});
-
-//On connection
-io.on('connection', function(socket){
-    console.log('New connection established!! : %s users connected!', (connections.length+1));
-    connections.push(socket);
-    //On disconnect
-    socket.on('disconnect', function(){
-        connections.splice(connections.indexOf(socket),1);
-        console.log('Connection dropped!! : %s users connected!', connections.length);
-    });
-    //Sending messages
-    socket.on('chat message', function(msg)
-    {
-        console.log('Message: '+msg);
-        io.emit('chat message', msg);
-    });
-});
-
-http.listen(3000, function() {
-    console.log('Listening on port 3000');
-});*/
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
