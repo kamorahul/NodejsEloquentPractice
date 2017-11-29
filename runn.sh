823b03e6fd951f9662420f827c3c01f94d420069
gurtaj-singh <33083016+gurtaj-singh@users.noreply.github.com>
2017-11-29 11:22:36 +0530

    Update runn.sh

--- runn.sh
+++ runn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 TIMESTAMP=`date +%F-%H%M`
-tar -zcvf /home/liverpoolfc/Desktop/dump/$TIMESTAMP.tgz /home/liverpoolfc/Desktop/mongo
+tar -zcvf /home/liverpoolfc/Desktop/dump/$TIMESTAMP.tgz /mongo 
 
